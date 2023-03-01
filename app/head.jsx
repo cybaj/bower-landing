bb9f3a24a8df3b270bd20d7e8edeff4cb54f52f2
cybaj <cybaj@naver.com>
2023-03-01 13:44:04 +0900

    first commit

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>바우어</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="바우어 입니다." />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
